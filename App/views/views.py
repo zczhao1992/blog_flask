460d7c90667b5695197ee0c8d53fe101d1b6e4b7
zczhao1992 <zczhao1992@163.com>
2024-08-15 15:04:13 +0800

    feat: 增加首页

--- App/views/views.py
+++ App/views/views.py
@@ -1,12 +1,54 @@
 """ 路由 """
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 from ..models.models import *
 
 # 蓝图
 blog = Blueprint('blog', __name__)
 
 
+# 博客首页
 @blog.route('/')
-def index():
+@blog.route('/index/')
+def blog_index():
     """ 首页 """
-    return 'index'
+    photos = photoModel.query.limit(6)
+    categorys = CategoryModel.query.all()
+    articles = ArticleModel.query.all()
+    commends = articles[:4]
+    return render_template('home/index.html',
+                           photos=photos,
+                           categorys=categorys,
+                           articles=articles,
+                           commends=commends
+                           )
+
+
+# 博客相册
+@blog.route('/photos/')
+def blog_photos():
+    """ 相册 """
+    photos = photoModel.query.all()
+    return render_template('home/photos.html', photos=photos)
+
+
+# 博客日记
+@blog.route('/article/')
+def blog_article():
+    """ 日记 """
+    categorys = CategoryModel.query.all()
+    articles = ArticleModel.query.all()
+    return render_template('home/article.html',
+                           categorys=categorys,
+                           articles=articles)
+
+
+# 关于我
+@blog.route('/about/')
+def blog_about():
+    """ 关于我 """
+    photos = photoModel.query.limit(6)
+    categorys = CategoryModel.query.all()
+    return render_template('home/about.html',
+                           photos=photos,
+                           categorys=categorys
+                           )
--- App/views/views_admin.py
+++ App/views/views_admin.py
@@ -1,12 +1,23 @@
 """ 后台路由 """
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 from ..models.models_admin import *
 
 # 蓝图
 admin = Blueprint('admin', __name__)
 
 
-@admin.route('/')
+# 后台管理-首页
+@admin.route('/admin/')
 def index():
     """ 首页 """
-    return 'index'
+    return render_template("admin/index.html")
+
+
+# 后台管理-登录
+@admin.route('/admin/login/', methods=["GET", "POST"])
+def admin_login():
+    """ 登录 """
+    if request.method == 'GET':
+        return render_template("admin/login.html")
+    elif request.method == 'POST':
+        pass
