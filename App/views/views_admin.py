7985a510158d00e71181734a19d3d1943026709c
zczhao1992 <zczhao1992@163.com>
2024-08-18 20:02:16 +0800

    feat: 增加分类修改删除

--- App/views/views_admin.py
+++ App/views/views_admin.py
@@ -1,6 +1,6 @@
 """ 后台路由 """
 from functools import wraps
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, jsonify
 from ..models.models_admin import *
 from ..models.models import *
 
@@ -114,4 +114,82 @@ def admin_category():
     #     # 如果没有登录，则跳转到登录页面
     #     return redirect("/admin/login/")
     user = request.user
-    return render_template('admin/category.html', username=user.name)
+    categorys = CategoryModel.query.all()
+    return render_template('admin/category.html',
+                           username=user.name,
+                           categorys=categorys)
+
+
+# 后台管理-添加分类
+@admin.route("/admin/addcategory/", methods=["GET", "POST"])
+@login_required
+def admin_add_category():
+    """ 添加分类 """
+    if request.method == 'POST':
+        name = request.form.get("name")
+        describe = request.form.get("describe")
+
+        category = CategoryModel()
+        category.name = name
+        category.describe = describe
+
+        try:
+            db.session.add(category)
+            db.session.commit()
+        except Exception as e:
+            print(e)
+            db.session.rollback()
+
+        return redirect('/admin/category/')
+
+    return '请求方式错误！'
+
+
+# 后台管理-删除分类
+@admin.route("/admin/delcategory/", methods=["GET", "POST"])
+@login_required
+def admin_del_category():
+    """ 删除分类 """
+    if request.method == 'POST':
+        id = request.form.get('id')
+        category = CategoryModel.query.get(id)
+
+        try:
+            db.session.delete(category)
+            db.session.commit()
+        except Exception as e:
+            print(e)
+
+        return jsonify({"code": 200, "msg": '删除成功'})
+
+    return jsonify({"code": 400, "msg": '请求方式错误'})
+
+
+# 后台管理-修改分类
+@admin.route("/admin/updatecategory/<id>/", methods=["GET", "POST"])
+@login_required
+def admin_update_category(id):
+    """ 修改分类 """
+    if request.method == 'GET':
+        user = request.user
+        category = CategoryModel.query.get(id)
+        return render_template('admin/category_update.html',
+                               username=user.name,
+                               category=category
+                               )
+    elif request.method == 'POST':
+        name = request.form.get("name")
+        describe = request.form.get("describe")
+
+        category = CategoryModel.query.get(id)
+        category.name = name
+        category.describe = describe
+
+        try:
+            db.session.commit()
+        except Exception as e:
+            print(e)
+
+        return redirect('/admin/category/')
+    else:
+        return '请求方式错误'
